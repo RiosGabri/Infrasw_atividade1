6b8cf03f9e6d8ae0591979e6e8279252ced613c0
RiosGabri <grp@cesar.school>
2026-08-19 08:46:44 -0300

    refactor: leitura simples do terminal no main

--- processflow.c
+++ processflow.c
@@ -1,18 +1,13 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <string.h>
 
-int main(int argc, char *argv[]) {
-/*    FILE *arquivo = fopen(argv[1], "r");
-    if (arquivo == NULL) {
-        printf("Erro ao abrir o arquivo!\n");
-        return 1;
-    }
+//int argc, char *argv[]
+int main() {
+    char linha[81];
 
-    char linha[256];
-    while (fgets(linha, sizeof(linha), arquivo) != NULL) {
+    if (fgets(linha, sizeof(linha), stdin) != NULL) {
         printf("%s", linha);
     }
 
-    fclose(arquivo);*/
     return 0;
 }
